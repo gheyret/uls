fb1ee30b43f557a53b7e0025c096a8cc42ebaac4
kawichi <gheyret@gmail.com>
2017-05-31 14:35:01 +0900

    Add files via upload

---
+++ uls_word/ulsdelete.bat
@@ -0,0 +1,4 @@
+ECHO OFF
+SET dest=%USERPROFILE%\AppData\Roaming\Microsoft\Word\STARTUP\uls.dotm
+del %dest%
+set dest=
---
+++ uls_word/ulssetup.bat
@@ -0,0 +1,4 @@
+ECHO OFF
+SET dest=%USERPROFILE%\AppData\Roaming\Microsoft\Word\STARTUP\uls.dotm
+copy uls.dotm %dest% /Y
+set dest=
